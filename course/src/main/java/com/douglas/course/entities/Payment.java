ac19b6b8b2d3a4739fbfdf06ce9936480bb3c99a
devbruto93 <douglas.modest.2013@gmail.comd>
2025-04-19 14:33:10 -0300

    Payment, ono-to-one association

---
+++ course/src/main/java/com/douglas/course/entities/Payment.java
@@ -0,0 +1,58 @@
+package com.douglas.course.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+
+    @OneToOne
+    @MapsId
+    private Order order;
+
+    public Payment(){
+
+    }
+
+    public Payment(Long id, Instant moment, Order o1) {
+        this.id = id;
+        this.moment = moment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Payment payment)) return false;
+        return Objects.equals(id, payment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+}
